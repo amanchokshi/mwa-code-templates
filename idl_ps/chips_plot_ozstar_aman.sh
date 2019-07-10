097e4642e2372dc26d45c485c86a1c03debff6b0
Aman Chokshi <achokshi@student.unimelb.edu.au>
2019-07-10 13:29:34 +1000

    Script to rotate output pngs

--- idl_ps/chips_plot_ozstar_aman.sh
+++ idl_ps/chips_plot_ozstar_aman.sh
@@ -8,6 +8,7 @@ echo yy_0.iter.${1} >> idl_batch_aman
 echo 1 >> idl_batch_aman
 echo $1 >> idl_batch_aman
 
+# Creates new script to rotate idl png images by 180 deg
 cat > rotate$1.sh <<EOF
 for file in ./$1/*.png; do
     convert "\$file" -rotate 180 "\${file%.png}".png
